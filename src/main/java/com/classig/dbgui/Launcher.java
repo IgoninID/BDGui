4fbe1d9e0e91261adfd21e4d67d0d18b1b30a48b
IgoninID <vova.igonin1050@gmail.com>
2025-05-14 15:11:40 +0900

    launcher

---
+++ src/main/java/com/classig/dbgui/Launcher.java
@@ -0,0 +1,10 @@
+package com.classig.dbgui;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args)
+    {
+        Application.launch(DBApplication.class, args);
+    }
+}
